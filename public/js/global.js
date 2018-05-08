832d466efe809a80e2fe8287efe3337e5118b170
mittalprince <princemittal766@gmail.com>
2018-05-08 22:12:59 +0530

    make it better

--- public/js/global.js
+++ public/js/global.js
@@ -25,10 +25,9 @@ $(document).ready(function(){
 
                 var userName =users[i].name
 
-                var list = '<img src="https://placehold.it/300x300" class="pull-left img-circle" ' +
-                    'style="width:50px; margin-right:10px; "/><p>' +
-                    '<a id="val" href="/chat/'+username+'.'+name+'"><h3 style="padding-top: 15px;color:gray; font-size: 14px;">'+'@'+ users[i].name+'' +
-                    '<span class="fa fa-circle online_friend"></span></h3></a></p> '
+                var list = '<img src="https://placehold.it/300x300" class="pull-left img-circle" style="width:50px; height:50px; margin-right:10px;" /><p>' +
+                    '<a id="val" href="/chat/'+userName.replace(/ /g, "-")+'.'+name.replace(/ /g, "-")+'"><h3 style="padding-top:15px;color:gray; font-size:14px;">'+'@'+users[i].name+'<span class="fa fa-circle online_friend"></span></h3></a></p>' +
+                    '<div class="clearfix"></div><hr style=" margin-top: 14px; margin-bottom: 14px;" />'
                 ol.append(list);
             }
         }
